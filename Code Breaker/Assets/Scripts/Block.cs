fea1ccc08bc618ba0dd598507aa36d39c43956f4
ArthurGonze <arthur.gonze@ice.ufjf.br>
2019-02-21 11:19:16 -0300

    Breakout game with 5 levels. Need some adjustments on the ball movement

---
+++ Code Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //config params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites; 
+    
+    // cached reference
+    Level level;
+    GameStatus gameStatus;
+
+    // state variables
+    [SerializeField] int timesHit = 0; //only serialized for debug purposes
+
+    private void Start()
+    {
+        CountBlocks();
+
+        gameStatus = FindObjectOfType<GameStatus>();
+    }
+
+    private void CountBlocks()
+    {
+        level = FindObjectOfType<Level>();
+
+        if (tag == "Breakable")
+        {
+            level.addOneBreakableBlock();
+        }
+        else if (tag == "Unbreakable")
+        {
+            level.addOneUnbreakableBlock();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+
+        int maxHits = hitSprites.Length;
+
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        GetComponent<SpriteRenderer>().sprite = hitSprites[timesHit];
+    }
+
+    private void DestroyBlock()
+    {
+        TriggerSparklesVFX();// produz efeito de particula para destruicao do bloco
+        Destroy(gameObject);//destroi o objeto
+        playBlockDestroySFX();//produz som de colisao
+        gameStatus.AddToScore(); // aumenta a pontuacao do jogador
+        level.removeOneBreakableBlock(); // diminui em 1 o numero total de blocos da fase
+    }
+
+    private void playBlockDestroySFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
---
+++ Code Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // configuration paramaters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // cached references
+    GameStatus theGameStatus;
+    Ball theBall;
+
+    //configuration
+    Vector2 initialPosition = new Vector2(8f,0.4f);
+
+    // Use this for initialization
+    void Start()
+    {
+        theGameStatus = FindObjectOfType<GameStatus>();
+        theBall = FindObjectOfType<Ball>();
+        transform.position = initialPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameStatus.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
